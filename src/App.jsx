24e99c78f7571c994672331ecf12165f970acb3b
simone-poli <polismn97@gmail.com>
2025-07-23 15:57:24 +0200

    add form onSubmit with function onclick button send to API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 
 function App() {
+  const url = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
 
   const [formData, setFormData] = useState({
     author: '',
@@ -11,21 +12,45 @@ function App() {
   })
 
 
-  function handleFormData(e){
+  function handleFormData(e) {
     const key = e.target.name
-    const val = e.target.value === 'public' ? e.target.checked : e.target.value 
+    const val = e.target.value === 'public' ? e.target.checked : e.target.value
 
 
     setFormData({
       ...formData,
-      [key] : val
+      [key]: val
     })
+  }
+
+
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+
+      .catch(error => console.error('errore', error))
+
+
   }
 
 
   return (
     <>
-      
+      <form onSubmit={handleSubmit}>
         <div className="container">
           <h1 className="text-center">Post Form</h1>
           <div className="card">
@@ -35,6 +60,7 @@ function App() {
               <input
                 type="text"
                 className="form-control"
+                value={formData.author}
                 name="author"
                 id="author"
                 placeholder="Get here the author"
@@ -43,12 +69,13 @@ function App() {
             </div>
 
 
-              {/* title section */}
+            {/* title section */}
             <div className="p-3">
               <label className="form-label">Title</label>
               <input
                 type="text"
                 className="form-control"
+                value={formData.title}
                 name="title"
                 id="title"
                 placeholder="Get here the post title"
@@ -57,44 +84,44 @@ function App() {
             </div>
 
 
-              {/* text post section */}
+            {/* text post section */}
             <div className="m-3">
               <label className="form-label">Content Post</label>
-              <textarea className="form-control" name="body" id="body" rows="3" onChange={handleFormData}></textarea>
+              <textarea className="form-control" value={formData.body} name="body" id="body" rows="3" onChange={handleFormData}></textarea>
             </div>
-            
-            
 
-          
+
+
+
             <div className="form-check m-3">
               <input
                 className="form-check-input"
                 type="checkbox"
                 name='public'
-                value="public"
+                value={formData.public}
                 id="public"
                 onChange={handleFormData}
               />
               <label className="form-check-label"> Public </label>
             </div>
-            
 
 
-            
+
+
           </div>
-            <div className="d-grid gap-2 mt-3">
-              <button
-                type="button"
-                name=""
-                id=""
-                className="btn btn-primary"
-              >
-                Send
-              </button>
-            </div>
-            
+          <div className="d-grid gap-2 mt-3">
+            <button
+              type="submit"
+              name=""
+              id=""
+              className="btn btn-primary"
+            >
+              Send
+            </button>
+          </div>
+
         </div >
-     
+      </form>
     </>
   )
 }
